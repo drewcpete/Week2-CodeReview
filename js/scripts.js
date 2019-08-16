b103c5edd7eb7420a2d883772d503b940c10aee7
drewcpete@gmail.com <drewcpete@gmail.com>
2019-08-16 11:43:42 -0700

    adds function to js

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,18 @@ $(document).ready(function() {
     console.log("test");
     var material = parseInt($("#question1").val());
     var emotion = parseInt($("#question2").val());
-    debugger;
-    var alcohol = parseInt($("question3").val());
-    var sport = parseInt($("question4").val());
-    var currency = parseInt($("question5").val());
-    var topping = parseInt($("question6").val());
-    var avocado = parseInt($("question7").val());
+    var alcohol = parseInt($("#question3").val());
+    var sport = parseInt($("#question4").val());
+    var currency = parseInt($("#question5").val());
+    var topping = parseInt($("#question6").val());
+    var avocado = parseInt($("#question7").val());
     console.log(material);
-    var result = (material + emotion + alcohol + sport + currency + topping + avocado) % 4;
+    var result = (material + emotion + alcohol + sport + currency + topping + avocado);
+
     console.log(result);
+    result = (result % 4)
+    console.log(result)
+    debugger;
 
     if (result === 0){
       $("#python").show();
